4f26f32f496f09c8ee41b816117df50d4796da41
Vlad-Stefan Sas <vlad.sas@student.unitbv.ro>
2023-10-26 18:10:34 +0300

    feat: (Console Client) added FileHandler class to provide easier access to various data
    - at the moment it only works with the Dictionary file; LogFiles and Database readers/writers need to be implemented

---
+++ Gartic/Gartic/FileHandler.h
@@ -0,0 +1,22 @@
+#pragma once
+#include <vector>
+#include <fstream>
+#include <string>
+#include <random>
+import Config;
+
+class FileHandler
+{
+public:
+	enum FileType
+	{
+		Dictionary,
+		Database,
+		LogFile
+	};
+
+	static void write(FileType fileType, const std::string& data);
+	static std::string read(FileType fileType);
+	FileHandler() = delete;
+};
+
