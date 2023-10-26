5cf0c047b9de8d2881e3c94e12435ac61e6a45ea
tdennis <teglasdennis@yahoo.com>
2023-10-26 12:18:40 +0300

    Added LoginScene and added funtionality for ConsoleManager

--- Gartic/Gartic/ConsoleManager.h
+++ Gartic/Gartic/ConsoleManager.h
@@ -1,3 +1,5 @@
+#include <string>
+#include <iostream>
 #include <Windows.h>
 
 #include "ColorType.h"
@@ -7,11 +9,14 @@ class ConsoleManager
 {
 public:
 	static void Initialize();
+	static void ClearScreen();
 	static void SetConsoleScale(int x, int y);
-	static void GoToXY(int x, int y);
+	static void SetCursor(int x, int y);
 	static void SetBackgroundColor(ColorType color);
 	static void SetTextColor(ColorType color);
 	static void SetColor(ColorType background, ColorType text);
+	static void WriteVertical(const std::string &sentence, int x, int y);
+	static void WriteHorizontal(const std::string &sentence, int x, int y);
 private:
 	static HANDLE m_h;
 	static int m_color;
---
+++ Gartic/Gartic/LoginScene.h
@@ -0,0 +1,28 @@
+#include <string>
+
+#include "ConsoleManager.h"
+
+#pragma once
+class LoginScene
+{
+public:
+	static void SetActive();
+private:
+	static std::string m_username;
+	static std::string m_password;
+
+	enum Options
+	{
+		USER,
+		PASSWORD,
+		LOGIN,
+		REGISTER
+	};
+	static Options m_option;
+
+	static void Start();
+	static void Update();
+	static void Display();
+	static bool Login();
+	static bool Register();
+};
