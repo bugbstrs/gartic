fc96e2512a5307ea7ac3ca62e1f0957654989490
AndreiTufeanu <andrei.tufeanu@student.unitbv.ro>
2023-10-26 19:11:11 +0300

    Added Player class including its constructors and points management functions

---
+++ Gartic/Gartic/Player.cpp
@@ -0,0 +1,21 @@
+#include "Player.h"
+
+Player::Player() : 
+	m_user(User()), 
+	m_points(0)
+{}
+
+Player::Player(const User &user) : 
+	m_user(user), 
+	m_points(0)
+{}
+
+uint16_t Player::GetPoints() const
+{
+	return m_points;
+}
+
+void Player::AddPoints(int nrOfPointsToBeAdded)
+{
+	m_points += nrOfPointsToBeAdded;
+}
