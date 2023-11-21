2c9628cba6a9acb47a3f371692bdb07a4c8db220
Teglas Dennis <teglasdennis@yahoo.com>
2023-11-21 10:04:36 +0200

    [SelectableObject] Added implementation for SelectableObject

---
+++ Console Client/ConsoleClient/SelectableObject.cpp
@@ -0,0 +1,31 @@
+module SelectableObject;
+
+SelectableObject::SelectableObject(COORD upLeftCorner, Align align, ColorType backgroundColor, ColorType textColor,
+                                   int16_t max_width, ColorType selectedBackgroundColor, ColorType selectedTextColor):
+    GUIObject                 { upLeftCorner, align, backgroundColor, textColor, max_width },
+	m_selectedBackgroundColor { selectedBackgroundColor                                    },
+	m_selectedTextColor		  { selectedTextColor		                                   }
+{}
+
+SelectableObject::SelectableObject(int16_t x, int16_t y, Align align, ColorType backgroundColor, ColorType textColor,
+                                   int16_t max_width, ColorType selectedBackgroundColor, ColorType selectedTextColor):
+    GUIObject                 { x, y, align, backgroundColor, textColor, max_width },
+    m_selectedBackgroundColor { selectedBackgroundColor                            },
+    m_selectedTextColor       { selectedTextColor                                  }
+{}
+
+bool SelectableObject::IsPointInside(COORD point)
+{
+    if (m_upLeftCorner.X >= point.X && m_upLeftCorner.X + m_width < point.X &&
+        m_upLeftCorner.Y >= point.Y && m_upLeftCorner.Y + m_height < point.Y)
+        return true;
+    return false;
+}
+
+bool SelectableObject::IsPointInside(int16_t x, int16_t y)
+{
+    if (m_upLeftCorner.X >= x && m_upLeftCorner.X + m_width < x &&
+        m_upLeftCorner.Y >= y && m_upLeftCorner.Y + m_height < y)
+        return true;
+    return false;
+}
