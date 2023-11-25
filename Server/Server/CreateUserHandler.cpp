027282ced8979d8ac5b10bc3f79e06a29bbdc39b
Vlad-Stefan Sas <vlad.sas@student.unitbv.ro>
2023-11-25 21:12:05 +0200

    fix/feat: (Server) retrieve params by querystring & error handling

--- Server/Server/CreateUserHandler.cpp
+++ Server/Server/CreateUserHandler.cpp
@@ -6,18 +6,36 @@ http::CreateUserHandler::CreateUserHandler(GarticStorage& storage): m_db(storage
 
 crow::response http::CreateUserHandler::operator()(const crow::request& req) const
 {
-	auto bodyArgs = ParseUrlArgs(req.body);
-	auto end = bodyArgs.end();
-	auto username = bodyArgs.find("username");
-	auto password = bodyArgs.find("password");
-	auto points = bodyArgs.find("points");
-	auto gamesPlayed = bodyArgs.find("games played");
-	
-	if (gamesPlayed != end && points != end && username != end && password != end)
-	{
-		m_db.CreateUser(std::stoi(gamesPlayed->second), std::stoi(points->second), username->second, password->second);
-		return crow::response(201); 
-	}
-	
-	return crow::response(401);
+    const crow::query_string& bodyArgs    = req.url_params;
+    char*                     gamesPlayed = bodyArgs.get("games played");
+    char*                     password    = bodyArgs.get("password");
+    char*                     points      = bodyArgs.get("points");
+    char*                     username    = bodyArgs.get("username");
+    
+    if (gamesPlayed == nullptr || password == nullptr)
+    {
+        // todo: log
+        return crow::response(401);
+    }
+    if (points == nullptr || username == nullptr)
+    {
+        // todo: log
+        return crow::response(400);
+    }
+
+    try
+    {
+        m_db.CreateUser(std::stoi(gamesPlayed), std::stoi(points), String(username), String(password));
+        return crow::response(201);
+    }
+    catch (std::invalid_argument invalidArgumentException)
+    {
+        // todo: log with useful info
+        return crow::response(400);
+    }
+    catch (std::out_of_range outOfRangeException)
+    {
+        // todo: log with useful info
+        return crow::response(400);
+    }
 }
