79f23ddb7d0cd50180df7f073718fdc8491628c4
Vlad-Stefan Sas <vlad.sas@student.unitbv.ro>
2023-11-26 02:35:24 +0200

    feat: (Server) initial testing code

--- Server/Server/Source Files/Source.cpp
+++ Server/Server/Source Files/Source.cpp
@@ -5,10 +5,22 @@
 
 #include "Server/RouteManager.h"
 
+#define DOCTEST_CONFIG_IMPLEMENT
+#include "doctest/doctest.h";
+
 using namespace http;
 
-int main()
+int main(int argc, char** argv)
 {
+    if (argc == 2 && strcmp("test", argv[1]) == 0)
+    {
+        doctest::Context ctx;
+        ctx.setOption("abort-after", 1);
+        ctx.applyCommandLine(argc, argv);
+        ctx.setOption("no-breaks", true);
+        return ctx.run();
+    }
+
 	GarticStorage storage;
 	if (!storage.Initialize())
 	{
