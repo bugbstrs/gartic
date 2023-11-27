d8c1a12a7589eac161d33fd7ff57cea23d747ebf
tdennis <teglasdennis@yahoo.com>
2023-11-28 00:37:37 +0200

    [SelectableObject] Can now call outside functions

--- Console Client/ConsoleClient/SelectableObject.cpp
+++ Console Client/ConsoleClient/SelectableObject.cpp
@@ -2,44 +2,54 @@ module SelectableObject;
 
 SelectableObject::SelectableObject(COORD upLeftCorner, Align align, ColorType backgroundColor,
                                    ColorType textColor, int16_t maxWidth, int16_t maxHeight,
-                                   ColorType selectedBackgroundColor,
-                                   ColorType selectedTextColor, SelectableObject*& selected) :
-    GUIObject                 { upLeftCorner, align, backgroundColor, textColor, maxWidth, maxHeight },
-	m_selectedBackgroundColor { selectedBackgroundColor                                              },
-	m_selectedTextColor		  { selectedTextColor		                                             },
-    m_selectedObject          { selected                                                             },
-    m_hoverBackgroundColor    { backgroundColor                                                      },
-    m_hoverTextColor          { textColor                                                            }
+                                   ColorType selectedBackgroundColor, ColorType selectedTextColor,
+                                   ConsoleManager* cm, InputManager* im, SelectableObject*& selected) :
+    GUIObject                 { upLeftCorner, align, backgroundColor, textColor, maxWidth, maxHeight, cm },
+	m_selectedBackgroundColor { selectedBackgroundColor                                                  },
+	m_selectedTextColor		  { selectedTextColor		                                                 },
+    m_selectedObject          { selected                                                                 },
+    m_hoverBackgroundColor    { backgroundColor                                                          },
+    m_hoverTextColor          { textColor                                                                },
+    m_im                      { im                                                                       }
 {}
 
 SelectableObject::SelectableObject(int16_t x, int16_t y, Align align, ColorType backgroundColor,
                                    ColorType textColor, int16_t maxWidth, int16_t maxHeight,
                                    ColorType selectedBackgroundColor,
-                                   ColorType selectedTextColor, SelectableObject*& selected) :
-    GUIObject                 { x, y, align, backgroundColor, textColor, maxWidth, maxHeight },
-    m_selectedBackgroundColor { selectedBackgroundColor                                      },
-    m_selectedTextColor       { selectedTextColor                                            },
-    m_selectedObject          { selected                                                     },
-    m_hoverBackgroundColor    { backgroundColor                                              },
-    m_hoverTextColor          { textColor                                                    }
+                                   ColorType selectedTextColor, ConsoleManager* cm, 
+                                   InputManager* im, SelectableObject*& selected) :
+    GUIObject                 { x, y, align, backgroundColor, textColor, maxWidth, maxHeight, cm },
+    m_selectedBackgroundColor { selectedBackgroundColor                                          },
+    m_selectedTextColor       { selectedTextColor                                                },
+    m_selectedObject          { selected                                                         },
+    m_hoverBackgroundColor    { backgroundColor                                                  },
+    m_hoverTextColor          { textColor                                                        },
+    m_im                      { im                                                               }
 {}
 
 SelectableObject::SelectableObject(Align align, ColorType backgroundColor, ColorType textColor,
                                    int16_t maxWidth, int16_t maxHeight,
                                    ColorType selectedBackgroundColor, ColorType selectedTextColor,
-                                   SelectableObject * &selected) :
-    GUIObject                 { align, backgroundColor, textColor, maxWidth, maxHeight },
-    m_selectedBackgroundColor { selectedBackgroundColor                                },
-    m_selectedTextColor       { selectedTextColor                                      },
-    m_selectedObject          { selected                                               },
-    m_hoverBackgroundColor    { backgroundColor                                        },
-    m_hoverTextColor          { textColor                                              }
+                                   ConsoleManager* cm, InputManager* im,
+                                   SelectableObject*& selected) :
+    GUIObject                 { align, backgroundColor, textColor, maxWidth, maxHeight, cm },
+    m_selectedBackgroundColor { selectedBackgroundColor                                    },
+    m_selectedTextColor       { selectedTextColor                                          },
+    m_selectedObject          { selected                                                   },
+    m_hoverBackgroundColor    { backgroundColor                                            },
+    m_hoverTextColor          { textColor                                                  },
+    m_im                      { im                                                         }
 {}
 
 void SelectableObject::SetHoverColors(ColorType background, ColorType text)
 {
     m_hoverBackgroundColor = background;
     m_hoverTextColor       = text;
+}
+
+void SelectableObject::SetFunctionOnActivate(Function function)
+{
+    m_function = function;
 }
 
 void SelectableObject::SetConections(SelectableObject* up, SelectableObject* down,
@@ -49,14 +59,6 @@ void SelectableObject::SetConections(SelectableObject* up, SelectableObject* dow
     m_downObject  = down;
     m_leftObject  = left;
     m_rightObject = right;
-}
-
-void SelectableObject::CheckInput(InputManager* im)
-{
-    if (this == m_selectedObject)
-    {
-        
-    }
 }
 
 bool SelectableObject::IsPointInside(COORD point)
--- Console Client/ConsoleClient/SelectableObject.cppm
+++ Console Client/ConsoleClient/SelectableObject.cppm
@@ -1,6 +1,7 @@
 export module SelectableObject;
 
 export import <Windows.h>;
+import <functional>;
 
 export import InputManager;
 export import ColorType;
@@ -8,27 +9,33 @@ export import GUIObject;
 
 export typedef short int16_t;
 
+using Function = std::function<void()>;
+
 export class SelectableObject : public GUIObject
 {
 public:
     // Constructors
     SelectableObject(COORD upLeftCorner, Align align, ColorType backgroundColor, ColorType textColor,
                      int16_t maxWidth, int16_t maxHeight, ColorType selectedBackgroundColor,
-                     ColorType selectedTextColor, SelectableObject*& selected);
+                     ColorType selectedTextColor, ConsoleManager* cm, InputManager* im,
+                     SelectableObject*& selected);
     SelectableObject(int16_t x, int16_t y, Align align, ColorType backgroundColor, ColorType textColor,
                      int16_t maxWidth, int16_t maxHeight, ColorType selectedBackgroundColor,
-                     ColorType selectedTextColor, SelectableObject*& selected);
+                     ColorType selectedTextColor, ConsoleManager* cm, InputManager* im,
+                     SelectableObject*& selected);
     SelectableObject(Align align, ColorType backgroundColor, ColorType textColor, int16_t maxWidth,
                      int16_t maxHeight, ColorType selectedBackgroundColor, ColorType selectedTextColor,
-                     SelectableObject*& selected);
+                     ConsoleManager* cm, InputManager* im, SelectableObject*& selected);
 
     // Destructor
     virtual ~SelectableObject() = default;
 
-    void SetHoverColors (ColorType background, ColorType text);
-    void SetConections  (SelectableObject* up, SelectableObject* down,
-                         SelectableObject* left, SelectableObject* right);
-    void CheckInput     (InputManager* im);
+    void SetHoverColors        (ColorType background, ColorType text);
+    void SetFunctionOnActivate (Function function);
+    void SetConections         (SelectableObject* up, SelectableObject* down,
+                                SelectableObject* left, SelectableObject* right);
+
+    virtual void CheckInput () = 0;
     
 protected:
     ColorType         m_selectedBackgroundColor;
@@ -40,6 +47,8 @@ protected:
     SelectableObject* m_downObject;
     SelectableObject* m_leftObject;
     SelectableObject* m_rightObject;
+    InputManager*     m_im;
+    Function          m_function;
 
     bool IsPointInside(COORD point);
 };
