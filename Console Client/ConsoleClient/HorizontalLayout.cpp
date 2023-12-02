2b9442ebc0ff4cf49d191e8e7c9e6e80ba025329
tdennis <teglasdennis@yahoo.com>
2023-12-02 22:27:36 +0200

    [HorizontalLayout] Added a Constructor and a Destructor, added function for drawing, adding objects and set position for objects

--- Console Client/ConsoleClient/HorizontalLayout.cpp
+++ Console Client/ConsoleClient/HorizontalLayout.cpp
@@ -1,5 +1,9 @@
 module HorizontalLayout;
 
+import <string>;
+
+using String = std::string;
+
 HorizontalLayout::HorizontalLayout(COORD upLeftCorner, Align align, ColorType backgroundColor,
 								   int16_t maxWidth, int16_t maxHeight, ConsoleManager* cm,
 								   int space) :
@@ -14,24 +18,68 @@ HorizontalLayout::HorizontalLayout(int16_t x, int16_t y, Align align, ColorType 
 	m_space   { space																   }
 {}
 
+HorizontalLayout::HorizontalLayout(Align align, ColorType backgroundColor, int16_t maxWidth,
+								   int16_t maxHeight, ConsoleManager *cm, int space):
+	GUIObject { align, backgroundColor, backgroundColor, maxWidth, maxHeight, cm },
+	m_space	  { space															 }
+{}
+
+HorizontalLayout::~HorizontalLayout()
+{
+	for (auto obj : m_objects)
+		delete obj;
+}
+
+void HorizontalLayout::Draw()
+{
+	SetColor();
+
+	DrawBackground();
+
+	DrawContents();
+}
+
+void HorizontalLayout::AddObjects(std::vector<GUIObject*> objects)
+{
+	for (auto obj : objects)
+		AddObject(obj);
+}
+
 void HorizontalLayout::AddObject(GUIObject* object)
 {
-	COORD upLeft = m_upLeftCorner;
-	COORD downRight = { upLeft.X + m_width, upLeft.Y + m_height };
+	if (object->GetHeight() > m_height || object->GetWidth() > m_width)
+		return;
+
+	int16_t rightPoint{ object->GetWidth() };
 	if (!m_objects.empty())
-	{
-		upLeft    = m_objects[m_objects.size() - 1]->GetUpLeftCorner();
-		upLeft.X += m_space;
-		downRight = m_objects[m_objects.size() - 1]->GetDownRightCorner();
-	}
-	upLeft.X += downRight.X;
-	upLeft.Y += downRight.Y;
-	while (downRight.X > m_upLeftCorner.X + m_width - 1 && !m_objects.empty())
+		rightPoint = m_objects[m_objects.size() - 1]->GetWidth() +
+					 object->GetWidth() - 1 + m_space;
+
+	while (rightPoint > m_width)
 	{
 		m_objects.erase(m_objects.begin());
+		SetObjectsPosition();
+
+		rightPoint = m_objects[m_objects.size() - 1]->GetWidth() +
+					 object->GetWidth() - 1 + m_space;
 	}
-	
+
 	m_objects.push_back(object);
-	
-	object->InitializeTransform();
+	SetObjectsPosition();
+}
+
+void HorizontalLayout::DrawContents()
+{
+	for (auto obj : m_objects)
+		obj->Draw();
+}
+
+void HorizontalLayout::SetObjectsPosition()
+{
+	COORD pos{m_upLeftCorner};
+	for (auto object : m_objects)
+	{
+		object->InitializeTransform(pos);
+		pos.X += object->GetWidth();
+	}
 }
