49daa56596125e128d6cdac573ade55468578a8a
tdennis <teglasdennis@yahoo.com>
2023-12-02 22:32:28 +0200

    [InputField] Added implementation for a Constructor and refactored the code

--- Console Client/ConsoleClient/InputField.cpp
+++ Console Client/ConsoleClient/InputField.cpp
@@ -21,72 +21,75 @@ InputField::InputField(int16_t x, int16_t y, Align align, ColorType backgroundCo
 	m_text			 { text															}
 {}
 
+InputField::InputField(Align align, ColorType backgroundColor, ColorType textColor,
+					   int16_t maxWidth, int16_t maxHeight,
+					   ColorType selectedBackgroungColor, ColorType selectedTextColor,
+					   ConsoleManager *cm, InputManager *im,
+					   SelectableObject *&selected, String &text):
+	SelectableObject { align, backgroundColor, textColor, maxWidth, maxHeight		 ,
+					   selectedBackgroungColor, selectedTextColor, cm, im, selected },
+	m_text			 { text															}
+{}
+
 void InputField::Draw()
 {
-	if (this == m_selectedObject)
-	{
-		m_cm->SetColor(m_selectedBackgroundColor, m_selectedTextColor);
+	SetColor();
 
-		COORD cursorPos = m_upLeftCorner;
-		cursorPos.X    += m_textPos % m_width;
-		cursorPos.Y    += m_textPos / m_width;
-		m_cm->SetCursor(true, cursorPos);
-	}else
-	{
-		if (IsPointInside(m_im->GetCurrentCursorPosition()))
-			m_cm->SetColor(m_hoverBackgroundColor, m_hoverTextColor);
-		else
-			m_cm->SetColor(m_backgroundColor, m_textColor);
-	}
+	DrawBackground();
 
-	int index{ 0 };
-	while (index < m_text.size())
-	{
-		m_cm->Write(m_text[index], m_upLeftCorner.X + index % m_width,
-								   m_upLeftCorner.Y + index / m_width);
-		++index;
-	}
+	DrawContents();
 }
 
 void InputField::CheckInput()
 {
-	if (this == m_selectedObject)
+	switch (m_im->ControlKey())
 	{
-		switch (m_im->ControlKey())
-		{
-		case ControlKeys::Enter:
-			if (m_function)
-				m_function();
-			break;
-		case ControlKeys::UpArrow:
-			if (m_textPos / m_width > 0)
-				m_textPos -= m_width;
-			else
-				m_selectedObject = m_upObject;
-			break;
-		case ControlKeys::DownArrow:
-			if (m_textPos < m_width * (m_height - 1))
-				m_textPos += m_width;
-			else
-				m_selectedObject = m_downObject;
-			break;
-		case ControlKeys::LeftArrow:
-			if (m_textPos > 0)
-				m_textPos--;
-			break;
-		case ControlKeys::RightArrow:
-			if (m_textPos + 1 < m_width * m_height)
-				m_textPos++;
-			break;
-		default:
-			break;
-		}
-	}
-	if (IsPointInside(m_im->GetCurrentCursorPosition()) &&
-		m_im->GetClickPressed())
-	{
-		m_selectedObject = this;
+	case ControlKeys::Enter:
 		if (m_function)
 			m_function();
+		break;
+	case ControlKeys::UpArrow:
+		if (m_textPos / m_width > 0)
+			m_textPos -= m_width;
+		else
+			if(m_upObject)
+				m_selectedObject = m_upObject;
+		break;
+	case ControlKeys::DownArrow:
+		if (m_textPos < m_width * (m_height - 1))
+			m_textPos += m_width;
+		else
+			if (m_downObject)
+				m_selectedObject = m_downObject;
+		break;
+	case ControlKeys::LeftArrow:
+		if (m_textPos > 0)
+			m_textPos--;
+		break;
+	case ControlKeys::RightArrow:
+		if (m_textPos + 1 < m_width * m_height)
+			m_textPos++;
+		break;
+	default:
+		break;
+	}
+}
+
+void InputField::DrawContents()
+{
+	if (this == m_selectedObject)
+	{
+		COORD cursorPos{m_upLeftCorner};
+		cursorPos.X += m_textPos % m_width;
+		cursorPos.Y += m_textPos / m_width;
+		m_cm->SetCursor(true, cursorPos);
+	}
+
+	int index{0};
+	while (index < m_text.size())
+	{
+		m_cm->Write(m_text[index], m_upLeftCorner.X + index % m_width,
+					m_upLeftCorner.Y + index / m_width);
+		++index;
 	}
 }
