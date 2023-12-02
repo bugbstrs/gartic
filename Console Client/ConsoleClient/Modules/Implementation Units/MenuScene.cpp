7c7f7983a734c31607e05c11add05a93a600826f
tdennis <teglasdennis@yahoo.com>
2023-12-02 22:57:12 +0200

    [MenuScene] Recreated the login scene with the new GUIObjects

--- Console Client/ConsoleClient/Modules/Implementation Units/MenuScene.cpp
+++ Console Client/ConsoleClient/Modules/Implementation Units/MenuScene.cpp
@@ -3,6 +3,11 @@
 import <Windows.h>;
 
 import Label;
+import Button;
+import InputField;
+import HorizontalLayout;
+
+using Color = ColorType;
 
 MenuScene::MenuScene(ConsoleManager* console, InputManager* inputManager) :
     Scene{ console, inputManager }
@@ -16,17 +21,71 @@ void MenuScene::Display() const
     m_console->UpdateConsole();
 }
 
+void MenuScene::Input() const
+{
+    m_input->ReadInput();
+
+    for (auto object : m_selectableObjects)
+        object->CheckCursor();
+
+    m_selected->CheckInput();
+}
+
 void MenuScene::Start()
 {
     m_nextScene = nullptr;
-    m_console->NewConsole(L"Menu", 200, 40);
-    m_objects.emplace_back(new Label{ 1, 1, Align::Left, ColorType::Blue, ColorType::Red,
-                                      5, 10, m_console, "Acesta este un test" });
+    m_console->NewConsole(L"Menu", 60, 15);
+
+    m_objects.emplace_back(new Label{30, 2, Align::Left, Color::Cyan, Color::Magenta, 6, 1, m_console, "GARTIC"});
+
+    auto layout{new HorizontalLayout{20, 5, Align::Left, Color::Black, 23, 1, m_console, 0}};
+    layout->AddObject(new Label{Align::Left, Color::Black, Color::White, 5, 1, m_console, "User:"});
+    auto userField{new InputField{Align::Left, Color::Gray, Color::Black, 18, 1, Color::Blue, Color::White,
+                        m_console, m_input, m_selected, m_username}};
+    layout->AddObject(userField);
+    m_selectableObjects.emplace_back(userField);
+
+    m_objects.emplace_back(layout);
+
+    layout = new HorizontalLayout{16, 6, Align::Left, Color::Black, 27, 1, m_console, 0};
+    layout->AddObject(new Label{Align::Left, Color::Black, Color::White, 9, 1, m_console, "Password:"});
+    auto passwordField{new InputField{Align::Left, Color::Gray, Color::Black, 18, 1, Color::Blue, Color::White,
+                        m_console, m_input, m_selected, m_password}};
+    layout->AddObject(passwordField);
+    m_selectableObjects.emplace_back(passwordField);
+
+    m_objects.emplace_back(layout);
+
+    auto loginButton{new Button{25, 10, Align::Left, Color::DarkGray, Color::Green, 5, 1, Color::Blue,
+                        Color::White, m_console, m_input, m_selected, "LOGIN"}};
+    loginButton->SetHoverColors(Color::DarkBlue, Color::White);
+    //loginButton->SetFunctionOnActivate();
+    m_objects.emplace_back(loginButton);
+    m_selectableObjects.emplace_back(loginButton);
+
+    auto registerButton{new Button{36, 10, Align::Left, Color::DarkGray, Color::Cyan, 8, 1, Color::Blue,
+                        Color::White, m_console, m_input, m_selected, "REGISTER"}};
+    registerButton->SetHoverColors(Color::DarkBlue, Color::White);
+    //loginButton->SetFunctionOnActivate();
+    m_objects.emplace_back(registerButton);
+    m_selectableObjects.emplace_back(registerButton);
+
+    userField->SetConections(nullptr, passwordField, nullptr, nullptr);
+    passwordField->SetConections(userField, loginButton, nullptr, nullptr);
+    loginButton->SetConections(passwordField, nullptr, nullptr, registerButton);
+    registerButton->SetConections(passwordField, nullptr, loginButton, nullptr);
+
+    delete m_selected;
+    m_selected = userField;
+
     Display();
 }
 
 void MenuScene::Update()
 {
     while (true)
+    {
+        Input();
         Display();
+    }
 }
