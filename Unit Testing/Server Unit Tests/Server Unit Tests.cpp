b7277a4c92d8220ef68f8249541053cf65aeb822
Vlad-Stefan Sas <vlad.sas@student.unitbv.ro>
2023-12-09 15:16:54 +0200

    feat: added initial sln/project files for server unit testing

---
+++ Unit Testing/Server Unit Tests/Server Unit Tests.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace ServerUnitTests
+{
+	TEST_CLASS(ServerUnitTests)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
