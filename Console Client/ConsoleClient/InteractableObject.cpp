c069b8d5ca0534c1575b2bd6ab093b39bb5de66d
tdennis <teglasdennis@yahoo.com>
2023-12-10 20:34:07 +0200

    [InteractableObject] Added implementation for checking if a point is inside the object and set the colors

--- Console Client/ConsoleClient/InteractableObject.cpp
+++ Console Client/ConsoleClient/InteractableObject.cpp
@@ -1,2 +1,17 @@
 module InteractableObject;
 
+bool InteractableObject::IsPointInside(COORD point)
+{
+    if (m_upLeftCorner.X <= point.X && m_upLeftCorner.X + m_width > point.X &&
+        m_upLeftCorner.Y <= point.Y && m_upLeftCorner.Y + m_height > point.Y)
+        return true;
+    return false;
+}
+
+void InteractableObject::SetColor()
+{
+    if (IsPointInside(m_im->GetCurrentCursorPosition()))
+        m_cm->SetColor(m_hoverBackgroundColor, m_hoverTextColor);
+    else
+        m_cm->SetColor(m_backgroundColor, m_textColor);
+}
