9a082da2d20da9a6f83fc219eda51aaef7ebb818
AndreiTufeanu <andrei.tufeanu@student.unitbv.ro>
2023-12-17 16:41:24 +0200

    [GUI] Improved DrawingBoard fill time

--- GUI Client/DrawingBoard.cpp
+++ GUI Client/DrawingBoard.cpp
@@ -102,125 +102,46 @@ void DrawingBoard::ClearCanvas()
 
 void DrawingBoard::FloodFill(QPoint startingPoint, QPoint pointToExecuteAt, QColor startingColor, QColor colorToBeFilledWith)
 {
-    /*bool topDone = false;
+    bool topDone = false;
     bool bottomDone = false;
     bool leftDone = false;
-    bool rightDone = false;*/
-    /*image.setPixelColor(startingPoint, colorToBeFilledWith);*/
+    bool rightDone = false;
+
+    image.setPixelColor(startingPoint, colorToBeFilledWith);
+
     QPoint currentPointTop = pointToExecuteAt + QPoint(0, 1);
     QPoint currentPointBottom = pointToExecuteAt + QPoint(0, -1);
     QPoint currentPointLeft = pointToExecuteAt + QPoint(-1, 0);
     QPoint currentPointRight = pointToExecuteAt + QPoint(1, 0);
 
-    std::thread topFill = std::thread(&DrawingBoard::GenericFill, this, currentPointTop, std::ref(currentPointTop), startingColor, colorToBeFilledWith, std::ref(availableDirections["topDone"]));
+    int centerX = startingPoint.x();
+    int centerY = startingPoint.y();
+    int radius1 = 25;
+    int radius2 = 26;
 
-    std::thread bottomFill = std::thread(&DrawingBoard::GenericFill, this, currentPointBottom, std::ref(currentPointBottom), startingColor, colorToBeFilledWith, std::ref(availableDirections["bottomDone"]));
+    std::vector <std::thread> threads;
 
-    std::thread leftFill = std::thread(&DrawingBoard::GenericFill, this, currentPointLeft, std::ref(currentPointLeft), startingColor, colorToBeFilledWith, std::ref(availableDirections["leftDone"]));
-
-    std::thread rightFill = std::thread(&DrawingBoard::GenericFill, this, currentPointRight, std::ref(currentPointRight), startingColor, colorToBeFilledWith, std::ref(availableDirections["rightDone"]));
-
-    /*std::thread topFill = std::thread(&DrawingBoard::FillTop, this, startingPoint + QPoint(0, 1), pointToExecuteAt + QPoint(0, 1), std::ref(currentPointTop), startingColor, colorToBeFilledWith);
-
-    std::thread bottomFill = std::thread(&DrawingBoard::FillBottom, this, startingPoint + QPoint(0, -1), pointToExecuteAt + QPoint(0, -1), std::ref(currentPointBottom), startingColor, colorToBeFilledWith);
-
-    std::thread leftFill = std::thread(&DrawingBoard::FillLeft, this, startingPoint + QPoint(-1, 0), pointToExecuteAt + QPoint(-1, 0), std::ref(currentPointLeft), startingColor, colorToBeFilledWith);
-
-    std::thread rightFill = std::thread(&DrawingBoard::FillRight, this, startingPoint + QPoint( 1, 0), pointToExecuteAt + QPoint(1, 0), std::ref(currentPointRight),  startingColor, colorToBeFilledWith);*/
-
-    //while (true) {
-    //    if (topFill.joinable() && !availableDirections["topDone"]) {
-    //        availableDirections["topDone"] = true;
-    //        topFill.join();
-    //        std::thread newThread1(&DrawingBoard::FillBottom, this, currentPointBottom, currentPointBottom, std::ref(currentPointBottom), startingColor, colorToBeFilledWith);
-    //        if (newThread1.joinable())
-    //            newThread1.join();
-    //        //newThread1.detach();
-    //    }
-    //    if (bottomFill.joinable() && !availableDirections["bottomDone"]) {
-    //        availableDirections["bottomDone"] = true;
-    //        bottomFill.join();
-    //        std::thread newThread2(&DrawingBoard::FillTop, this, currentPointTop, currentPointTop, std::ref(currentPointTop), startingColor, colorToBeFilledWith);
-    //        if (newThread2.joinable())
-    //            newThread2.join();
-    //        //newThread2.detach();
-    //    }
-    //    if (leftFill.joinable() && !availableDirections["leftDone"]) {
-    //        availableDirections["leftDone"] = true;
-    //        leftFill.join();
-    //        std::thread newThread3(&DrawingBoard::FillRight, this, currentPointRight, currentPointRight, std::ref(currentPointRight), startingColor, colorToBeFilledWith);
-    //        if (newThread3.joinable())
-    //            newThread3.join();
-    //        //newThread3.detach();
-    //    }
-    //    if (rightFill.joinable() && !availableDirections["rightDone"]) {
-    //        availableDirections["rightDone"] = true;
-    //        rightFill.join();
-    //        std::thread newThread4(&DrawingBoard::FillLeft, this, currentPointLeft, currentPointLeft, std::ref(currentPointLeft), startingColor, colorToBeFilledWith);
-    //        if (newThread4.joinable())
-    //            newThread4.join();
-    //        //newThread4.detach();
-    //    }
-    //    if (!topFill.joinable() && !bottomFill.joinable() && !leftFill.joinable() && !rightFill.joinable())
-    //        break;
-    //}
-    //topFill.join();
-    //bottomFill.join();
-    //leftFill.join();
-    //rightFill.join();
-
-    auto getAvailableDirection = [this, &currentPointTop, &currentPointBottom, &currentPointLeft, &currentPointRight]() -> QPoint& {
-        if (availableDirections["topDone"])
-            return currentPointTop;
-        if (availableDirections["bottomDone"])
-            return currentPointBottom;
-        if (availableDirections["leftDone"])
-            return currentPointLeft;
-        if (availableDirections["rightDone"])
-            return currentPointRight;
-    };
-    //De modifcat bool urile cu .joinable() in lambda
-    while (true) {
-        if (!topFill.joinable() && !bottomFill.joinable() && !leftFill.joinable() && !rightFill.joinable())
-            break;
-        /*QPoint& pixelToStartFilling = getAvailableDirection();*/
-        if (availableDirections["topDone"] && topFill.joinable()) {
-            availableDirections["topDone"] = false;
-            topFill = std::thread(&DrawingBoard::GenericFill, this, currentPointBottom, std::ref(currentPointBottom), startingColor, colorToBeFilledWith, std::ref(availableDirections["topDone"]));
+    // Create the diamond shape
+    for (int i = -radius2; i <= radius2; i++) {
+        for (int j = -radius2; j <= radius2; j++) {
+            int x = centerX + i;
+            int y = centerY + j;
+            if (image.pixelColor(QPoint(x, y)) != startingColor && image.pixelColor(QPoint(x, y)) != colorToBeFilledWith)
+                break;
+            double distance = sqrt(i * i + j * j);
+            QPoint pointToExecuteAt(x, y);
+            if (distance >= radius1 && distance <= radius2) {
+                threads.emplace_back(std::thread(&DrawingBoard::GenericFill, this, QPoint(x, y), std::ref(pointToExecuteAt), startingColor, colorToBeFilledWith, std::ref(availableDirections["topDone"])));
+            }
         }
-
-        if (availableDirections["bottomDone"] && bottomFill.joinable()) {
-            availableDirections["bottomDone"] = false;
-            bottomFill = std::thread(&DrawingBoard::GenericFill, this, currentPointTop, std::ref(currentPointTop), startingColor, colorToBeFilledWith, std::ref(availableDirections["bottomDone"]));
-        }
-
-        if (availableDirections["leftDone"] && leftFill.joinable()) {
-            availableDirections["leftDone"] = false;
-            leftFill = std::thread(&DrawingBoard::GenericFill, this, currentPointRight, std::ref(currentPointRight), startingColor, colorToBeFilledWith, std::ref(availableDirections["leftDone"]));
-        }
-
-        if (availableDirections["rightDone"] && rightFill.joinable()) {
-            availableDirections["rightDone"] = false;
-            rightFill = std::thread(&DrawingBoard::GenericFill, this, currentPointLeft, std::ref(currentPointLeft), startingColor, colorToBeFilledWith, std::ref(availableDirections["rightDone"]));
-        }
-        topFill.join();
-        bottomFill.join();
-        leftFill.join();
-        rightFill.join();
     }
-    availableDirections = {
-        {"topDone", false},
-        {"bottomDone", false},
-        {"leftDone", false},
-        {"rightDone", false}
-    };
+    for (auto& thread : threads) {
+        thread.join();
+    }
 }
 
 void DrawingBoard::GenericFill(QPoint startingPoint, QPoint& pointToExecuteAt, QColor startingColor, QColor colorToBeFilledWith, bool& done)
 {
-    if (!image.rect().contains(pointToExecuteAt))
-        return;
-
     if (image.pixelColor(pointToExecuteAt) != startingColor)
         return;
 
@@ -233,14 +154,15 @@ void DrawingBoard::GenericFill(QPoint startingPoint, QPoint& pointToExecuteAt, Q
         currentPoint = pointsQueue.front();
         pointsQueue.pop();
 
-        if (image.pixelColor(currentPoint) != startingColor || !imageRect.contains(currentPoint) || image.pixelColor(currentPoint) == colorToBeFilledWith)
+        if (image.pixelColor(currentPoint) != startingColor || image.pixelColor(currentPoint) == colorToBeFilledWith)
             continue;
 
         image.setPixelColor(currentPoint, colorToBeFilledWith);
-        pointsQueue.push(QPoint(currentPoint.x() + 1, currentPoint.y()));
+
         pointsQueue.push(QPoint(currentPoint.x(), currentPoint.y() + 1));
         pointsQueue.push(QPoint(currentPoint.x(), currentPoint.y() - 1));
         pointsQueue.push(QPoint(currentPoint.x() - 1, currentPoint.y()));
+        pointsQueue.push(QPoint(currentPoint.x() + 1, currentPoint.y()));
     }
     done = true;
     update();
