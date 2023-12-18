7cff3b28520101f1cc2b634e01465c92e71e160f
tdennis <teglasdennis@yahoo.com>
2023-12-18 13:55:40 +0200

    [GameScene] Started working on GameScene

--- Console Client/ConsoleClient/Modules/Implementation Units/GameScene.cpp
+++ Console Client/ConsoleClient/Modules/Implementation Units/GameScene.cpp
@@ -1,5 +1,7 @@
 module GameScene;
 
+using Color = ColorType;
+
 GameScene::GameScene(ConsoleManager *console, InputManager *inputManager):
 	Scene{console, inputManager}
 {}
@@ -29,17 +31,51 @@ void GameScene::GetDrawEvents()
 
 }
 
+void GameScene::Leave()
+{
+	m_nextScene = const_cast<std::type_info *>(&typeid(MenuScene));
+}
+
 void GameScene::Input() const
 {
+	m_input->ReadInput();
 
+	for (auto object : m_selectableObjects)
+		object->CheckCursor();
+
+	m_selected->CheckInput();
 }
 
 void GameScene::Start()
 {
+	m_nextScene = nullptr;
+	m_console->SetWindowed();
+	m_console->NewConsole(L"Game", 160, 90);
+	m_console->SetConsoleFont(L"MS Gothic", 12, 12);
+	m_console->SetFullscreen();
 
+	// Drawing Board  120x75
+	m_drawingBoard = new DrawingBoard{24, 5, 112, 70, Color::White, m_console, m_input, m_selected, 784, 490};
+	m_selectableObjects.emplace_back(m_drawingBoard);
+	m_objects.emplace_back(m_drawingBoard);
+
+	// Users layout
+	m_users = new VerticalLayout{2, 5, Align::Center, Align::Up, Color::White, 20, 70, m_console, 0};
+	m_objects.emplace_back(m_users);
+
+	// Chat layout
+	m_chat = new VerticalLayout{138, 5, Align::Center, Align::Up, Color::White, 20, 80, m_console, 0};
+	m_objects.emplace_back(m_chat);
+
+
+	m_selected = m_drawingBoard;
 }
 
 void GameScene::Update()
 {
-
+	while (m_nextScene == nullptr)
+	{
+		Input();
+		Display();
+	}
 }
--- Console Client/ConsoleClient/Modules/Interfaces/GameScene.cppm
+++ Console Client/ConsoleClient/Modules/Interfaces/GameScene.cppm
@@ -1,5 +1,8 @@
 export module GameScene;
 
+import VerticalLayout;
+import DrawingBoard;
+import MenuScene;
 import Scene;
 
 export class GameScene: public Scene
@@ -11,12 +14,16 @@ public:
 
 private:
 	SelectableObject *m_selected;
+	VerticalLayout	 *m_users;
+	VerticalLayout   *m_chat;
+	DrawingBoard	 *m_drawingBoard;
 
 	void GetPlayers		();
 	void GetTimer		();
 	void GetRound		();
 	void GetChat		();
 	void GetDrawEvents	();
+	void Leave			();
 	void Input			() const override;
 	void Start			()       override;
 	void Update			()       override;
