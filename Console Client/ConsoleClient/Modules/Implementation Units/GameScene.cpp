58ea7b0271a02e122fa0463b6d6510e93272192c
tdennis <teglasdennis@yahoo.com>
2023-12-19 23:43:45 +0200

    [GameScene] Added GUI

--- Console Client/ConsoleClient/Modules/Implementation Units/GameScene.cpp
+++ Console Client/ConsoleClient/Modules/Implementation Units/GameScene.cpp
@@ -26,6 +26,17 @@ void GameScene::GetChat()
 
 }
 
+void GameScene::SendMessage()
+{
+	//send message to server
+	Color color = m_lastChatColor ? Color::Gray : Color::White;
+	m_lastChatColor = !m_lastChatColor;
+	m_chat->AddObject(new Label{Align::Left, Align::Up, color, Color::Black, 20,
+					  static_cast<int16_t>((m_message.size() + 5) / 20 + 1), m_console, "You: " + m_message});
+
+	m_message.clear();
+}
+
 void GameScene::GetDrawEvents()
 {
 
@@ -49,26 +60,41 @@ void GameScene::Input() const
 void GameScene::Start()
 {
 	m_nextScene = nullptr;
+	m_message = "";
+	m_lastChatColor = false;
 	m_console->SetWindowed();
 	m_console->NewConsole(L"Game", 160, 90);
 	m_console->SetConsoleFont(L"MS Gothic", 12, 12);
 	m_console->SetFullscreen();
 
-	// Drawing Board  120x75
+	// Drawing Board
 	m_drawingBoard = new DrawingBoard{24, 5, 112, 70, Color::White, m_console, m_input, m_selected, 784, 490};
 	m_selectableObjects.emplace_back(m_drawingBoard);
 	m_objects.emplace_back(m_drawingBoard);
 
 	// Users layout
-	m_users = new VerticalLayout{2, 5, Align::Center, Align::Up, Color::White, 20, 70, m_console, 0};
+	m_users = new VerticalLayout{2, 5, Align::Center, Align::Up, Color::White, 20, 70, m_console, 1};
 	m_objects.emplace_back(m_users);
 
 	// Chat layout
-	m_chat = new VerticalLayout{138, 5, Align::Center, Align::Up, Color::White, 20, 80, m_console, 0};
+	m_chat = new VerticalLayout{138, 5, Align::Center, Align::Up, Color::White, 20, 77, m_console, 1};
 	m_objects.emplace_back(m_chat);
 
+	// Chatbox
+	auto chatbox = new InputField{138, 83, Align::Left, Align::Up, Color::Gray, Color::Black, 20, 5,
+								 Color::Gray, Color::Black, m_console, m_input, m_selected, m_message, 100};
+	chatbox->SetFunctionOnActivate(std::bind(&GameScene::SendMessage, this));
+	m_selectableObjects.emplace_back(chatbox);
+	m_objects.emplace_back(chatbox);
 
-	m_selected = m_drawingBoard;
+	// Leave button
+	auto leaveButton = new Button{2, 83, Align::Center, Align::Center, Color::Blue, Color::White, 11, 5,
+							Color::DarkBlue, Color::White, m_console, m_input, m_selected, "LEAVE"};
+	leaveButton->SetFunctionOnActivate(std::bind(&GameScene::Leave, this));
+	m_selectableObjects.emplace_back(leaveButton);
+	m_objects.emplace_back(leaveButton);
+
+	m_selected = leaveButton;
 }
 
 void GameScene::Update()
--- Console Client/ConsoleClient/Modules/Interfaces/GameScene.cppm
+++ Console Client/ConsoleClient/Modules/Interfaces/GameScene.cppm
@@ -1,9 +1,16 @@
 export module GameScene;
 
+import <string>;
+
 import VerticalLayout;
 import DrawingBoard;
+import InputField;
 import MenuScene;
+import Button;
+import Label;
 import Scene;
+
+using String = std::string;
 
 export class GameScene: public Scene
 {
@@ -17,11 +24,14 @@ private:
 	VerticalLayout	 *m_users;
 	VerticalLayout   *m_chat;
 	DrawingBoard	 *m_drawingBoard;
+	String			  m_message;
+	bool			  m_lastChatColor;//For alternating colors on chat
 
 	void GetPlayers		();
 	void GetTimer		();
 	void GetRound		();
 	void GetChat		();
+	void SendMessage	();
 	void GetDrawEvents	();
 	void Leave			();
 	void Input			() const override;
