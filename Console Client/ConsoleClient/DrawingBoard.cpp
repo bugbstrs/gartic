4f525c514e9c48b44398b0398cb6f2c71ab79c34
tdennis <teglasdennis@yahoo.com>
2023-12-19 23:43:24 +0200

    [DrawingBoard] Added Undo and Clear functions

--- Console Client/ConsoleClient/DrawingBoard.cpp
+++ Console Client/ConsoleClient/DrawingBoard.cpp
@@ -2,44 +2,67 @@ module DrawingBoard;
 
 DrawingBoard::DrawingBoard(COORD upLeftCorner,
 			int16_t maxWidth, int16_t maxHeight,
-			ColorType backgroungColor, ConsoleManager* cm,
+						   Color backgroungColor, ConsoleManager* cm,
 			InputManager* im, SelectableObject*& selected,
 			uint64_t canvasWidth, uint64_t canvasHeight) :
 	SelectableObject{ upLeftCorner, Align::Center, Align::Center,
-					  ColorType::Black, ColorType::Black, maxWidth, maxHeight,
-					  ColorType::Black, ColorType::Black, cm, im, selected    },
-	m_sectorWidth	{ static_cast<int>(canvasWidth / maxWidth)				  },
-	m_sectorHeight	{ static_cast<int>(canvasHeight / maxHeight)			  }
+					  Color::Black, Color::Black, maxWidth, maxHeight,
+					  Color::Black, Color::Black, cm, im, selected    },
+	m_sectorWidth	{ static_cast<int>(canvasWidth / maxWidth)		  },
+	m_sectorHeight	{ static_cast<int>(canvasHeight / maxHeight)	  },
+	m_option		{ false											  }
 {
 	m_canvases.emplace(canvasWidth, canvasHeight, backgroungColor);
+	m_selectable = false;
 }
 
 DrawingBoard::DrawingBoard(int16_t x, int16_t y,
 			int16_t maxWidth, int16_t maxHeight,
-			ColorType backgroungColor, ConsoleManager* cm,
+			Color backgroungColor, ConsoleManager* cm,
 			InputManager* im, SelectableObject*& selected,
 			uint64_t canvasWidth, uint64_t canvasHeight) :
 	SelectableObject{ x, y, Align::Center, Align::Center,
-					  ColorType::Black, ColorType::Black, maxWidth, maxHeight,
-					  ColorType::Black, ColorType::Black, cm, im, selected    },
-	m_sectorWidth	{ static_cast<int>(canvasWidth / maxWidth)				  },
-	m_sectorHeight	{ static_cast<int>(canvasHeight / maxHeight)			  }
+					  Color::Black, Color::Black, maxWidth, maxHeight,
+					  Color::Black, Color::Black, cm, im, selected    },
+	m_sectorWidth	{ static_cast<int>(canvasWidth / maxWidth)		  },
+	m_sectorHeight	{ static_cast<int>(canvasHeight / maxHeight)	  },
+	m_option		{ false											  }
 {
 	m_canvases.emplace(canvasWidth, canvasHeight, backgroungColor);
+	m_selectable = false;
 }
 
 DrawingBoard::DrawingBoard(int16_t maxWidth,
 			int16_t maxHeight,
-			ColorType backgroungColor, ConsoleManager* cm,
+			Color backgroungColor, ConsoleManager* cm,
 			InputManager* im, SelectableObject*& selected,
 			uint64_t canvasWidth, uint64_t canvasHeight) :
 	SelectableObject{ Align::Center, Align::Center,
-					  ColorType::Black, ColorType::Black, maxWidth, maxHeight,
-					  ColorType::Black, ColorType::Black, cm, im, selected    },
-	m_sectorWidth	{ static_cast<int>(canvasWidth / maxWidth)				  },
-	m_sectorHeight	{ static_cast<int>(canvasHeight / maxHeight)			  }
+					  Color::Black, Color::Black, maxWidth, maxHeight,
+					  Color::Black, Color::Black, cm, im, selected    },
+	m_sectorWidth	{ static_cast<int>(canvasWidth / maxWidth)		  },
+	m_sectorHeight	{ static_cast<int>(canvasHeight / maxHeight)	  },
+	m_option		{ false											  }
 {
 	m_canvases.emplace(canvasWidth, canvasHeight, backgroungColor);
+	m_selectable = false;
+}
+
+void DrawingBoard::Undo()
+{
+	if (m_canvases.size() > 1)
+		m_canvases.pop();
+}
+
+void DrawingBoard::Clear()
+{
+	while (m_canvases.size() > 1)
+		m_canvases.pop();
+}
+
+bool &DrawingBoard::GetOption()
+{
+	return m_option;
 }
 
 void DrawingBoard::Draw()
@@ -50,13 +73,11 @@ void DrawingBoard::Draw()
 	DrawContents();
 }
 
-void DrawingBoard::CheckInput()
-{
-	
-}
+void DrawingBoard::CheckInput(){} // Needs to be overrided
 
 void DrawingBoard::CheckCursor()
 {
+	static bool isWaitingToSave{ true };
 	static COORD prevPoz{ -1,-1 };
 	COORD cursorPos{ m_im->GetCurrentCursorPosition() };
 	if (IsPointInside(cursorPos) &&
@@ -64,17 +85,23 @@ void DrawingBoard::CheckCursor()
 	{
 		if (prevPoz.X != -1)
 		{
-			Canvas newCanvas{ m_canvases.top() };
-			newCanvas.DrawLine(m_sectorHeight * (prevPoz.Y - m_upLeftCorner.Y) + m_sectorHeight / 2,
+			if (isWaitingToSave)
+			{
+				isWaitingToSave = false;
+				m_canvases.emplace(m_canvases.top());
+			}
+			m_canvases.top().DrawLine(m_sectorHeight * (prevPoz.Y - m_upLeftCorner.Y) + m_sectorHeight / 2,
 							  m_sectorWidth * (prevPoz.X - m_upLeftCorner.X) + m_sectorWidth / 2,
 							  m_sectorHeight * (cursorPos.Y - m_upLeftCorner.Y) + m_sectorHeight / 2,
 							  m_sectorWidth * (cursorPos.X - m_upLeftCorner.X) + m_sectorWidth / 2,
 							  ColorType::Green, 6);
-			m_canvases.emplace(newCanvas);
 		}
 		prevPoz = cursorPos;
 		return;
 	}
+	if (prevPoz.X != -1)
+		isWaitingToSave = true;
+
 	prevPoz = { -1,-1 };
 }
 
@@ -88,7 +115,7 @@ void DrawingBoard::DrawContents()
 		}
 }
 
-void DrawingBoard::SetColor(ColorType color)
+void DrawingBoard::SetColor(Color color)
 {
 	m_cm->SetColor(color, color);
 }
--- Console Client/ConsoleClient/DrawingBoard.cppm
+++ Console Client/ConsoleClient/DrawingBoard.cppm
@@ -5,33 +5,40 @@ import <stack>;
 import SelectableObject;
 import Canvas;
 
+using Color = ColorType;
+
 export class DrawingBoard : public SelectableObject
 {
 public:
 	DrawingBoard(COORD upLeftCorner, int16_t maxWidth, int16_t maxHeight,
-				 ColorType backgroungColor, ConsoleManager* cm,
+				 Color backgroungColor, ConsoleManager* cm,
 				 InputManager* im, SelectableObject*& selected,
 				 uint64_t canvasWidth, uint64_t canvasHeight);
 	DrawingBoard(int16_t x, int16_t y, int16_t maxWidth, int16_t maxHeight,
-				 ColorType backgroungColor, ConsoleManager* cm,
+				 Color backgroungColor, ConsoleManager* cm,
 				 InputManager* im, SelectableObject*& selected,
 				 uint64_t canvasWidth, uint64_t canvasHeight);
 	DrawingBoard(int16_t maxWidth, int16_t maxHeight,
-				 ColorType backgroungColor, ConsoleManager* cm,
+				 Color backgroungColor, ConsoleManager* cm,
 				 InputManager* im, SelectableObject*& selected,
 				 uint64_t canvasWidth, uint64_t canvasHeight);
 
 	~DrawingBoard() = default;
 
+	void Undo		();
+	void Clear		();
+	bool &GetOption ();
 	void Draw		() override;
 	void CheckInput	() override;
 	void CheckCursor() override;
 
 private:
 	std::stack<Canvas> m_canvases;
-	int	   m_sectorWidth;
-	int	   m_sectorHeight;
+	Color			   m_color;
+	bool			   m_option;// false = pen | true = fill
+	int				   m_sectorWidth;
+	int				   m_sectorHeight;
 
 	void DrawContents() override;
-	void SetColor	 (ColorType color);
+	void SetColor	 (Color color);
 };
