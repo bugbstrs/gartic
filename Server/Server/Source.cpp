6907d5590a3a6c7871e569a0551b0a3ea207c096
Vlad-Stefan Sas <vlad.sas@student.unitbv.ro>
2023-12-23 16:49:13 +0200

    chore: (Server) removed DOCTEST remnants

--- Server/Server/Source.cpp
+++ Server/Server/Source.cpp
@@ -6,24 +6,13 @@
 #include "Server/RouteManager.h"
 #include "Game.h"
 
-//#define DOCTEST_CONFIG_IMPLEMENT
-//#include "doctest/doctest.h";
-
 using namespace http;
 
-int main(int argc, char** argv)
+int main()
 {
-    //if (argc == 2 && strcmp("test", argv[1]) == 0)
-    //{
-    //    doctest::Context ctx;
-    //    ctx.setOption("abort-after", 1);
-    //    ctx.applyCommandLine(argc, argv);
-    //    ctx.setOption("no-breaks", true);
-    //    return ctx.run();
-    //}
-
 	GarticStorage storage;
-	if (!storage.Initialize())
+	
+    if (!storage.Initialize())
 	{
 		std::cout << "[ERROR] Failed to initialize the database!";
 		return -1;
