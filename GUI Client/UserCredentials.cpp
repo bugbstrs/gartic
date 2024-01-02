3c85ebf7acc901b943ed3acf480b2ac855e947a1
AndreiTufeanu <andrei.tufeanu@student.unitbv.ro>
2024-01-02 19:08:58 +0200

    [GUI] Added UserCredentials class for global access to username and password

---
+++ GUI Client/UserCredentials.cpp
@@ -0,0 +1,28 @@
+#include "UserCredentials.h"
+#include <stdexcept>
+
+bool UserCredentials::credentialsSet = false;
+std::string UserCredentials::m_username = "";
+std::string UserCredentials::m_password = "";
+
+void UserCredentials::SetCredentials(const std::string& username, const std::string& password)
+{
+    if (credentialsSet)
+    {
+        throw std::logic_error("Credentials can only be set once.");
+    }
+
+    m_username = username;
+    m_password = password;
+    credentialsSet = true;
+}
+
+std::string UserCredentials::GerUsername()
+{
+    return m_username;
+}
+
+std::string UserCredentials::GetPassword()
+{
+    return m_password;
+}
---
+++ GUI Client/UserCredentials.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <string>
+#include <numeric>
+#include <sstream>
+#include <regex>
+#include <cpr/cpr.h>
+
+class UserCredentials
+{
+public:
+    static void SetCredentials(const std::string& username, const std::string& password);
+    // Static method to get the first string
+    static std::string GerUsername();
+
+    // Static method to get the second string
+    static std::string GetPassword();
+private:
+    UserCredentials() = default;
+    UserCredentials(const UserCredentials&) = delete;
+    UserCredentials& operator=(const UserCredentials&) = delete;
+    static std::string m_username;
+    static std::string m_password;
+    static bool credentialsSet;
+};
+
