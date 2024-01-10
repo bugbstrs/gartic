a8f86a0e7ffec33035737aef098930b673d5b4b8
tdennis <teglasdennis@yahoo.com>
2024-01-10 15:02:06 +0200

    [Server] Players now gets removed if they are not talking to the server + rounds now work

--- Server/Server/Game.cpp
+++ Server/Server/Game.cpp
@@ -11,11 +11,20 @@ Game::Game(std::vector<std::shared_ptr<Player>>&& newPlayers) :
 	m_board{ std::shared_ptr<DrawingBoard>{ new DrawingBoard(m_players) } },
 	m_round{ std::shared_ptr<Round>{ new Round(m_players, m_wordToGuess, m_settings.GetDrawTime()) } }
 {
-	/*for (auto& player : m_players)
+	auto removePlayerCallback = [this](const std::string& username)
 	{
-		auto removePlayerCallback = std::bind(&Game::RemovePlayer, this, player->GetName());
-		player->GetTime().SetMethodToCall(removePlayerCallback);
-	}*/
+		RemovePlayer(username);
+	};
+	for (auto& player : m_players)
+	{
+		player->GetTime()->SetMethodToCall(removePlayerCallback, player->GetName());
+	}
+
+	auto nextRoundCallback = [this]()
+	{
+		NextRound();
+	};
+	m_gameTime->SetMethodToCall(nextRoundCallback);
 }
 
 std::vector<std::shared_ptr<Player>>& http::Game::GetPlayers() noexcept
@@ -91,40 +100,44 @@ void Game::NextRound()
 		if(!player->GetGuessed())
 		{ 
 			player->AddPoints(-50);
+			player->SetTimeWhenGuessed(m_gameTime->GetDuration() / 1000);
 		}
 
-		player->SetGuessed(false);
-		
 		averageGuessTime += player->GetTimeWhenGuessed();
+		
+		player->SetGuessed(false);
 	}
 
-	averageGuessTime /= (m_players.size() - 2);
+	averageGuessTime /= (m_players.size() - 1);
 
 	if (averageGuessTime == m_gameTime->GetDuration() / 1000)
-	{
 		m_round->GetDrawer()->AddPoints(-100);
-	}
 	else
-	{
 		m_round->GetDrawer()->AddPoints((m_gameTime->GetDuration() / 1000 - averageGuessTime) * 100 / (m_gameTime->GetDuration() / 1000));
-	}
 
-	if (m_round->GetRoundNumber() == m_settings.GetRoundsNumber() - 1)
+
+	m_round->NextDrawer();
+
+
+	/*if (m_round->GetRoundNumber() == m_settings.GetRoundsNumber() - 1)
 	{
-		/*std::random_device				   rd;
+		std::random_device				   rd;
 		std::default_random_engine		   engine(rd());
 		auto							   count = isWordsEntity ? m_db.count<WordsEntity>() : m_db.count<QuotesEntity>();
-		std::uniform_int_distribution<int> distribution(0, count - 1);*/
+		std::uniform_int_distribution<int> distribution(0, count - 1);
 
         // remove srand
 		srand(static_cast<unsigned int>(time(0)));
 
 		int randomRoundIndex = rand();
 
-		/*m_specialRoundType = (RoundType)randomRoundIndex;
+		m_specialRoundType = (RoundType)randomRoundIndex;
 
-		m_status = GameStatus::SpecialRound;*/
-	}
+		m_status = GameStatus::SpecialRound;
+	}*/
+
+
+	m_gameTime->Reset();
 }
 
 void http::Game::RemovePlayer(const std::string& username)
@@ -132,6 +145,9 @@ void http::Game::RemovePlayer(const std::string& username)
 	auto isPlayerToRemove = [&username](const std::shared_ptr<Player> player) { return player->GetName() == username; };
 	if (auto it = std::find_if(m_players.begin(), m_players.end(), isPlayerToRemove); it != m_players.end())
 	{
+		// TODO: Check if it is drawer
+
+
 		m_players.erase(it);
 		return;
 	}
