e4d3d0afc2c6435fa6ec7c3836a215a67b68ba44
Stoica-David <stoicadavidioan@gmail.com>
2024-01-12 23:03:32 +0200

    [GUI Client] Added connection for route FetchQuoteRoute from the server.

--- GUI Client/QuotesPlainText.cpp
+++ GUI Client/QuotesPlainText.cpp
@@ -1,4 +1,6 @@
 #include "QuotesPlainText.h"
+#include "UserCredentials.h"
+#include <crow.h>
 
 QuotesPlainText::QuotesPlainText(QWidget *parent)
 	: QPlainTextEdit(parent)
@@ -10,5 +12,16 @@ QuotesPlainText::~QuotesPlainText()
 void QuotesPlainText::showEvent(QShowEvent * event)
 {
 	//De apelat ruta si transformat in QString (cu fromUtf8()) ca altfel plange daca i dai std::string la setPlainText
-	setPlainText("");
+
+	auto quote = cpr::Get(
+		cpr::Url{ "http://localhost:18080/fetchquote" },
+		cpr::Parameters{
+			{"username", UserCredentials::GetUsername()},
+			{"password", UserCredentials::GetPassword()}
+		}
+	);
+
+	auto quoteText = crow::json::load(quote.text);
+
+	setPlainText(QString::fromUtf8(std::string(quoteText["quote"]));
 }
