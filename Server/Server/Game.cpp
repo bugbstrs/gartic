1cb2af6d655bf60671f7d9ccf4937254fb024763
AndreiTufeanu <andrei.tufeanu@student.unitbv.ro>
2024-01-13 00:43:58 +0200

    [Server] Solved poor checking for banned words

--- Server/Server/Game.cpp
+++ Server/Server/Game.cpp
@@ -6,11 +6,11 @@ using namespace http;
 Game::Game(std::vector<std::shared_ptr<Player>>&& newPlayers, GarticStorage& storage, GameSettings& newGameSettings) :
 	m_players{ std::move(newPlayers) },
 	m_gameStatus { GameStatus::PickingWord },
+	m_settings{ newGameSettings },
 	m_gameTime{ std::shared_ptr<Time>{ new Time(m_settings.GetDrawTime() * 1000, false) } },
-	m_chat { std::shared_ptr<Chat>{ new Chat(m_players, m_wordToGuess, m_gameTime, m_gameStatus, m_storage) } },
+	m_chat { std::shared_ptr<Chat>{ new Chat(m_players, m_wordToGuess, m_gameTime, m_gameStatus, storage) } },
 	m_board{ std::shared_ptr<DrawingBoard>{ new DrawingBoard(m_players) } },
 	m_round{ std::shared_ptr<Round>{ new Round(m_players, m_wordToGuess, storage, m_gameTime, m_settings.GetWordCount(), m_gameStatus) } },
-	m_settings{ newGameSettings },
 	m_storage { storage }
 {
 	auto removePlayerCallback = [this](const std::string& username)
