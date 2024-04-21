099fa92c9e853b10a100d60adbffbb798a881890
AndreiTufeanu <andrei.tufeanu@student.unitbv.ro>
2024-04-21 16:49:59 +0300

    [GUI] Adjusted the time between requests for drawing board state

--- GUI Client/DrawingBoard.cpp
+++ GUI Client/DrawingBoard.cpp
@@ -247,7 +247,7 @@ void DrawingBoard::CheckForNewDrawEvents(std::atomic<bool>& stop)
 			}
 		}
 		update();
-		std::this_thread::sleep_for(std::chrono::milliseconds(100));
+		std::this_thread::sleep_for(std::chrono::milliseconds(150));
 	}
 	if (stop.load()) {
 
